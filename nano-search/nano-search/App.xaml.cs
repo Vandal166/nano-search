37cd05b30496e58f3b198e24cf55c8d3cb1fa8a8
[V]andal <119330699+Vandal166@users.noreply.github.com>
2025-06-20 17:21:38 +0200

    init

---
+++ nano-search/nano-search/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
+
+namespace NanoSearch;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private NotifyIcon _trayIcon;
+
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        
+        _trayIcon = new NotifyIcon
+        {
+            Icon = new System.Drawing.Icon("resources\\nano-search.ico"),
+            Visible = true,
+            Text = "nano-search"
+        };
+        
+        _trayIcon.MouseClick += TrayIcon_MouseClick; // on click
+        _trayIcon.DoubleClick += TrayIcon_DoubleClick; // on double click
+    }
+
+    private void TrayIcon_DoubleClick(object? sender, EventArgs e)
+    {
+        //show search window
+    }
+
+    private void TrayIcon_MouseClick(object? sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Right)
+        {
+            // Show context menu
+            // allow indexing
+            // exit
+        }
+    }
+    
+    private void ShowSearchUI() 
+    {
+        var window = new SearchWindow(); // TODO cache to save resources
+        window.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e) 
+    {
+        _trayIcon.Dispose();
+        base.OnExit(e);
+    }
+}
---
+++ nano-search/nano-search/SearchWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Forms.VisualStyles;
+using System.Windows.Input;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+
+namespace NanoSearch;
+
+public partial class SearchWindow : FluentWindow
+{
+    public SearchWindow()
+    {
+        InitializeComponent();
+        ApplicationThemeManager.Apply(
+            ApplicationTheme.Dark,                // Light or Dark theme
+            WindowBackdropType.Mica,             // Mica or Acrylic
+            updateAccent: true
+        );
+        
+        SystemThemeWatcher.Watch(this);
+        
+        // to center
+        Loaded += (s, e) => {
+            SearchBox.Focus();
+            Top = SystemParameters.WorkArea.Height / 2 - Height * 2;
+            Left = SystemParameters.WorkArea.Width / 2 - Width / 2;
+        };
+    }
+    
+    private void SearchBox_KeyDown(object sender, KeyEventArgs e) {
+        if (e.Key == Key.Escape) {
+            Close();
+        } else if (e.Key == Key.Enter) {
+            string query = SearchBox.Text;
+            // Run search and show results
+        }
+    }
+}
